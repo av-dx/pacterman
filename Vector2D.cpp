e56fbe1bf6fe048f78b1ebf4a1c022e0e315217a
av-dx <aashwinvaish1@gmail.com>
2020-07-04 21:19:10 +0530

    Added Utitlity Functions

--- Vector2D.cpp
+++ Vector2D.cpp
@@ -1,4 +1,5 @@
 #include "Vector2D.h"
+#include <math.h>
 
 Vector2D::Vector2D()
 {
@@ -17,4 +18,12 @@ Vector2D Vector2D::operator+(Vector2D &a)
 Vector2D Vector2D::operator-(Vector2D &a)
 {
     return Vector2D(x - a.x, y - a.y);
+}
+Vector2D Vector2D::absolute(Vector2D &a)
+{
+    return Vector2D(abs(a.x), abs(a.y));
+}
+Vector2D Vector2D::frac()
+{
+    return Vector2D(x - floor(x), y - floor(y));
 }
--- Vector2D.h
+++ Vector2D.h
@@ -7,4 +7,6 @@ struct Vector2D
     Vector2D(float a, float b);
     Vector2D operator+(Vector2D &a);
     Vector2D operator-(Vector2D &a);
+    Vector2D absolute(Vector2D &a);
+    Vector2D frac();
 };
